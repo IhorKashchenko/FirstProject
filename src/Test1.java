c88a837439e7b1fefdbf31b13a2b6ff36f94a05f
ihorkashchenko <ihor.kashchenko@gmail.com>
2020-10-09 14:09:06 -0700

    test if else

--- src/Test1.java
+++ src/Test1.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class Test1 {
     public static void main(String[] args) {
-        int rt = 10;
+        int rt = 0;
         int u = 3;
-        int res = rt % 8;
-
-        System.out.println(res);
+        int res = rt + u;
+        if (res >= 0) {
+            System.out.println("The number is ZERO");}
+        else {
+        System.out.println(10);}
     }
 }
 
