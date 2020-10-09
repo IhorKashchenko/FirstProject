5ad890956629f15b178a8d6d2302c83d4976afb2
ihorkashchenko <ihor.kashchenko@gmail.com>
2020-10-09 14:21:46 -0700

    test if else

--- src/Test1.java
+++ src/Test1.java
@@ -1,14 +1,15 @@
-import java.util.Scanner;
-
-public class Test1 {
+class IfElse {
     public static void main(String[] args) {
-        int rt = 0;
-        int u = 3;
-        int res = rt + u;
-        if (res >= 0) {
-            System.out.println("The number is ZERO");}
+        int number = +10;  // meniaesh + 10 i -10 i budet raznyi otvet
+
+        // checks if number is greater than 0	 
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        }
         else {
-        System.out.println(10);}
+            System.out.println("The number is not positive.");
+        }
+
+        System.out.println("This statement is always executed.");
     }
 }
-
