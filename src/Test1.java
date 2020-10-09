eff63dbd36e3829f63ff066c5b6a0e5131df73ad
Ihor Kashchenko <IhorK@altec-wa.com>
2020-10-09 10:33:47 -0700

    MyProject

---
+++ src/Test1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Test1 {
+    public static void main(String[] args) {
+        int rt = 10;
+        int u = 3;
+        int res = rt % 8;
+
+        System.out.println(res);
+    }
+}
+
