17b1b87f7ee55ed570effd6d28991d27dd7fdbb2
ihorkashchenko <ihor.kashchenko@gmail.com>
2020-10-09 13:48:22 -0700

    test

--- src/Shared.java
+++ src/Shared.java
@@ -5,14 +5,8 @@ public class Shared {
     public static void main(String[] args) {
       /*  Scanner num = new Scanner(System.in);
         int first, second, result;
-        System.out.println("Enter first num: ");
-        first = num.nextInt();
-        System.out.println("Enter second num: ");
-        second = num.nextInt();
-        result = first + second;
-        System.out.println("Result is - " + result);    */
-        String V = "Vitalii";
-        String D = "Dasha";
+
+
         String result = V + D;
 
 
