e4fe852967fb5866a1911fa3b9a55709d08d6103
ihorkashchenko <ihor.kashchenko@gmail.com>
2020-10-09 14:25:27 -0700

    test if else

--- src/Test1.java
+++ src/Test1.java
@@ -1,15 +1,35 @@
-class IfElse {
+class Number {
     public static void main(String[] args) {
-        int number = +10;  // meniaesh + 10 i -10 i budet raznyi otvet
 
-        // checks if number is greater than 0	 
-        if (number > 0) {
-            System.out.println("The number is positive.");
+        // declaring double type variables
+        Double n1 = -1.0, n2 = 4.5, n3 = 5.3, largestNumber;
+
+        // checks if n1 is greater than or equal to n2
+        if (n1 >= n2) {
+
+            // if...else statement inside the if block
+            // checks if n1 is greater than or equal to n3
+            if (n1 >= n3) {
+                largestNumber = n1;
+            }
+
+            else {
+                largestNumber = n3;
+            }
         }
         else {
-            System.out.println("The number is not positive.");
+
+            // if..else statement inside else block
+            // checks if n2 is greater than or equal to n3
+            if (n2 >= n3) {
+                largestNumber = n2;
+            }
+
+            else {
+                largestNumber = n3;
+            }
         }
 
-        System.out.println("This statement is always executed.");
+        System.out.println("The largest number is " + largestNumber);
     }
 }
